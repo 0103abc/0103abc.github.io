1f320e9e69b0b9772ca7dd2ddd7b93029b062d70
0103abc <accfad9@gmail.com>
2026-06-13 15:52:21 +0800

    Update solution_data.js

--- solution_data.js
+++ solution_data.js
@@ -1,4 +1,15 @@
+// 从下往上添加题解
+
 window.SOLUTION_ITEMS = [
+  {
+    title: "P14510 夜里亦始终想念着你 miss 题解",
+    href: "solutions/P15410.html",
+    date: "2026-06-13",
+    oj: "洛谷",
+    difficulty: "NOI/NOI+/CTSC",
+    tags: ["组合数学", "数据结构"],
+    summary: "暂无"
+  }, 
   {
     title: "P1001 A+B Problem 题解",
     href: "solutions/p1001.html",
@@ -8,15 +19,6 @@ window.SOLUTION_ITEMS = [
     tags: ["入门", "模拟"],
     summary: "最经典的 A+B Problem，适合测试提交环境。"
   },
-  {
-    title: "棋子序列题解",
-    href: "solutions/P15410.html",
-    date: "2026-06-13",
-    oj: "洛谷",
-    difficulty: "NOI/NOI+/CTSC",
-    tags: ["组合数学", "数据结构"],
-    summary: "暂无"
-  }, 
   // 新增题解时，在这里继续加：
   // ,
   // {
